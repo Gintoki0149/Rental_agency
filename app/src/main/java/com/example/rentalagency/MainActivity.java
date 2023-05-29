f1379a571fb9b4cbfe828713f8e67f8fdf645da7
Gintoki0149 <eyeseyes149@gmail.com>
2023-05-29 15:00:20 +0530

    signup process done

---
+++ app/src/main/java/com/example/rentalagency/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.rentalagency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button signin;
+    private Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signin = findViewById(R.id.sign_in);
+        signup = findViewById(R.id.sign_up);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/rentalagency/SignUpActivity.java
@@ -0,0 +1,60 @@
+package com.example.rentalagency;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText firstname;
+    EditText lastname;
+    EditText Email;
+    EditText phone;
+    EditText password;
+    Button Submit;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        firstname = findViewById(R.id.firstnameedittext);
+        lastname = findViewById(R.id.lastnameedittext);
+        Email = findViewById(R.id.emailedittext);
+        phone = findViewById(R.id.phonenumberedittext);
+        password = findViewById(R.id.pwdedittext);
+        Submit = findViewById(R.id.submit);
+        auth  = FirebaseAuth.getInstance();
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(firstname.getText()!=null && lastname.getText()!=null && Email.getText()!=null && phone.getText()!=null && password.getText()!=null){
+                    auth.createUserWithEmailAndPassword(Email.getText().toString(),password.getText().toString()).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(SignUpActivity.this,"Signup successful!!",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUpActivity.this,LandingPage.class));
+                                finish();
+                            }else{
+                                Toast.makeText(SignUpActivity.this,"Couldn't signup",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this,"All fields need to be filled.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
